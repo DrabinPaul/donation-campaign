597280d13a4f0098d5a70be40be391463b1bdaf2
DrabinPaul <drabinpaul1429@gmail.com>
2023-09-26 17:42:17 +0600

    the pie chart has been displayed, and the error page is now ready

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,50 @@
+import React, { PureComponent } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Legend } from 'recharts'; // Import Legend from 'recharts'
+import { getStoredDonation } from '../../utility/localstorage';
 
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const RADIAN = Math.PI / 180; // Define RADIAN
+
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {(percent * 100).toFixed(0)}%
+        </text>
+    );
+};
 
 const Statistics = () => {
+    const dontaion = getStoredDonation();
+    const data = [
+        { name: 'Total Donation', value: (12 - dontaion.length) },
+        { name: 'Your Donation', value: dontaion.length },
+    ];
+
     return (
         <div>
-            <h2>statistics page</h2>
+            <div className="flex items-center justify-center">
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={100}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Legend />
+                </PieChart>
+            </div>
         </div>
     );
 };
