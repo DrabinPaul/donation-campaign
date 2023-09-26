6cfd42d624768e3e632e7d225b5be7390d5765cc
DrabinPaul <drabinpaul1429@gmail.com>
2023-09-26 23:02:40 +0600

    implemented some final updates

--- src/components/Donation/Donation.jsx
+++ src/components/Donation/Donation.jsx
@@ -6,41 +6,46 @@ import Donated from "../Donated/Donated";
 
 const Donation = () => {
     const donations = useLoaderData();
-    const [appliedDonation, setappliedDonation] = useState([]);
+    const [appliedDonation, setAppliedDonation] = useState([]);
     const [seeAll, setSeeAll] = useState(4);
 
     useEffect(() => {
         const storedDonationIds = getStoredDonation();
         if (donations.length > 0) {
-            const appiledDonation = [];
+            const appliedDonationArray = [];
             for (const id of storedDonationIds) {
-                const donationapplied = donations.find(donationapplied => donationapplied.id === id);
-                if (donationapplied) {
-                    appiledDonation.push(donationapplied);
+                const donationApplied = donations.find(donation => donation.id === id);
+                if (donationApplied) {
+                    appliedDonationArray.push(donationApplied);
                 }
             }
-            setappliedDonation(appiledDonation);
-            console.log(donations, storedDonationIds, appiledDonation);
+            setAppliedDonation(appliedDonationArray);
+            console.log(donations, storedDonationIds, appliedDonationArray);
         }
-    }, [])
+    }, []);
+
     return (
-           <div>
-             <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5 mt-12 p-5 lg:p-0">
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-5 mt-12 p-5 lg:p-0">
                 {
                     appliedDonation?.slice(0, seeAll).map(donated => <Donated key={donated.id} donated={donated}></Donated>)
                 }
-   
             </div>
-                <div className="flex justify-center pt-5 pb-5">
-                <div className={seeAll === appliedDonation.length && 'hidden'}>
-                    <button
-                        onClick={() => setSeeAll(appliedDonation.length)}
-                        className="px-4 py-2 bg-[#009444] text-white font-medium rounded-lg">See All
+            <div className="flex justify-center pt-5 pb-5">
+                {appliedDonation.length > 3 && (
+                    <div>
+                        <button
+                            onClick={() => setSeeAll(appliedDonation.length)}
+                            className="px-4 py-2 bg-[#009444] text-white font-medium rounded-lg"
+                        >
+                            See All
                         </button>
-                </div>
-                </div>
-           </div>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 };
 
 export default Donation;
+
